88bead85449c0ea69fed19fa24fd114c4d8c41c5
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-03-22 08:29:26 -0300

    Curso

---
+++ bin/academia_do_flutter_curso.dart
@@ -0,0 +1,5 @@
+import 'package:academia_do_flutter_curso/academia_do_flutter_curso.dart' as academia_do_flutter_curso;
+
+void main(List<String> arguments) {
+  print('Hello world: ${academia_do_flutter_curso.calculate()}!');
+}
---
+++ test/academia_do_flutter_curso_test.dart
@@ -0,0 +1,8 @@
+import 'package:academia_do_flutter_curso/academia_do_flutter_curso.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
